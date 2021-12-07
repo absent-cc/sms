d6e2a331ac58a77d10e3d2cc57ef85d8664fd78d
Roshan Karim <roshan@rkarim.xyz>
2021-12-07 00:21:09 -0500

    made threads restart automatically

--- src/main.py
+++ src/main.py
@@ -17,37 +17,52 @@ username = cfg['textnow']['username']
 sckeys = [cfg['north']['key'], cfg['south']['key']]
 scsecrets = [cfg['north']['secret'], cfg['south']['secret']]
 
-# Define objects. 
-
-sms = sms(sid, csrf, username)
-ui = ui(sms) 
-absent = absence(sckeys, scsecrets)
-
-# Define date.
-
 date = datetime.now() - timedelta(hours=5)
 
 # Functions for threads.
 
-def sms_listener(sms, ui):
-    while True:
-        for msg in sms.receive():
-            print(msg)
-            ui.main(msg)
-        time.sleep(0.2)
+def threadwrapper(func):
+    def wrapper():
+        while True:
+            try:
+                func()
+            except BaseException as error:
+                print('abSENT - {!r}; restarting thread'.format(error))
+            else:
+                print('abSENT - Exited normally, bad thread, restarting')
+    return wrapper
 
-def sc_listener(absent, date):
+def sms_listener():
+    
+    txt = sms(sid, csrf, username)
+    txtui = ui(txt) 
+
     while True:
+        for msg in txt.receive():
+            print(msg)
+            txtui.main(msg)
+            time.sleep(0.2)
+
+def sc_listener():
+    
+    absent = absence(sckeys, scsecrets)
+
+    while True:
+        date = datetime.now() - timedelta(hours=5)
+
         print(absent.filter_absences_north(date))
         print("\n\n")
         print(absent.filter_absences_south(date))
         print("\n\n\n\n")
+        
         time.sleep(10)
 
 # Configure and start threads.
 
-schoology = threading.Thread(target=sms_listener, args=(sms, ui))
-textnow = threading.Thread(target=sc_listener, args=(absent, date))
+threads = {
+        'sc': threading.Thread(target=threadwrapper(sc_listener), name='sc listener'),
+        'sms': threading.Thread(target=threadwrapper(sms_listener), name='sms listener')
+}
 
-schoology.start()
-textnow.start()
+threads['sms'].start()
+threads['sc'].start()
