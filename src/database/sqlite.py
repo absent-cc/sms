fc2a01b7672c4e5fb0b5ca9491dcb5b43055d658
Kevin Yang <ky200617@gmail.com>
2021-12-09 16:49:33 -0500

    made changes to db

--- src/database/sqlite.py
+++ src/database/sqlite.py
@@ -5,14 +5,17 @@ from typing import Tuple, List
 # MAKE SURE TO FILTER NAMES TO PREVENT SQL INJECTION
 
 class databaseHandler():
-    id = 0
+    classes_id = 0
+    directory_id = 0
+
     def __init__(self, db_path="data/absent.db"):
         self.db_path = db_path
+        self.reset(db_path)
         self.connection = sqlite3.connect(self.db_path)
         self.cursor = self.connection.cursor()
         create_directory = """
         CREATE TABLE IF NOT EXISTS directory 
-        (id STRING PRIMARY KEY,
+        (user_id STRING PRIMARY KEY,
         number STRING, 
         first STRING,
         last STRING, 
@@ -27,18 +30,47 @@ class databaseHandler():
 
         create_classes = """
         CREATE TABLE IF NOT EXISTS classes 
-        (teacher_first STRING,
-        teacher_last STRING, 
-        FOREIGN KEY(number) REFERENCES directory(number)
+        (classes_id STRING PRIMARY KEY,
+        teacher_first STRING,
+        teacher_last STRING,
+        block STRING,
+        user_id STRING,
+        FOREIGN KEY(user_id)
+            REFERENCES directory(user_id)
+                ON UPDATE CASCADE
+                ON DELETE CASCADE)
         """
+
         self.cursor.execute(create_directory)
+        self.cursor.execute(create_classes)
 
-    def new_id(self):
-        databaseHandler.id += 1
-        return databaseHandler.id
+    def reset(self, db_path):
+        import os
+        if os.path.exists(db_path):
+            os.remove(db_path)
+            
+    def __repr__(self) -> str:
+        return self.print_directory() + self.print_classes()
 
+    def newDirectoryID(self):
+        databaseHandler.directory_id += 1
+        return databaseHandler.directory_id
+
+    def newClassID(self):
+        databaseHandler.classes_id += 1
+        return databaseHandler.classes_id
+    
     def checkIfInDirectory(self, student: Student) -> bool:
         command = f"""SELECT EXISTS(SELECT 1 FROM directory WHERE number='{student.number}' LIMIT 1)
+        """
+        result = self.cursor.execute(command).fetchone()
+        if result == (1,):
+            return True
+        return False
+    
+    def checkIfInClasses(self, teacher: Teacher) -> bool:
+        command = f"""
+        SELECT EXISTS(SELECT 1 FROM classes WHERE teacher_first='{teacher.first}' AND teacher_last='{teacher.last}' LIMIT 1)
         """
         result = self.cursor.execute(command).fetchone()
         if result == (1,):
@@ -58,7 +90,7 @@ class databaseHandler():
     def addStudentToDirectory(self, student: Student) -> bool:
         if not self.checkIfInDirectory(student):
             teachers = self.teacherTupleList(student)
-            new_id = self.new_id()
+            new_id = self.newDirectoryID()
         
             command = f"""
             INSERT INTO directory VALUES
@@ -108,19 +140,79 @@ class databaseHandler():
             return True
         return False
 
-    def getStudentFromDirectory(self, id=None, Number=None) -> Student:
+    def getStudentFromDirectory(self, id: int = None, Number: Number = None) -> Student:
         if id != None:
             command = f"""
             SELECT * FROM directory WHERE id='{id}'
             """
             self.cursor.execute(command)
             row = self.cursor.fetchone()
-            print(row)
-            # student = Student(row[1], row[2], row[3])
+            if row != None:
+                teachers = []
+                for teacher_names in range(4, len(row), 2):
+                    teacher = Teacher(row[teacher_names], row[teacher_names+1])
+                    teachers.append(teacher)
+                schedule = Schedule(teachers[0], teachers[1], teachers[2], teachers[3], teachers[4], teachers[5], teachers[6])
+                return Student(row[2], row[3], row[1], schedule, row[0])
+        elif Number != None:
+            command = f"""
+            SELECT * FROM directory WHERE number='{Number}'
+            """
+            self.cursor.execute(command)
+            row = self.cursor.fetchone()
+            if row != None:
+                teachers = []
+                for teacher_names in range(4, len(row), 2):
+                    teacher = Teacher(row[teacher_names], row[teacher_names+1])
+                    teachers.append(teacher)
+                schedule = Schedule(teachers[0], teachers[1], teachers[2], teachers[3], teachers[4], teachers[5], teachers[6])
+                return Student(row[2], row[3], row[1], schedule, row[0])
+        return None
 
+    def addTeacherToClasses(self, teacher: Teacher, block: str, number: Number = None) -> bool:
+        if number != None:
+            number_to_add = str(number)
+        else:
+            number_to_add = ""
 
-    def __repr__(self) -> str:
-        return self.print_directory() + self.print_classes()
+        if not self.checkIfInClasses(teacher):
+            new_id = self.newClassID()
+            command = f"""
+            INSERT INTO classes VALUES
+            ('{new_id}', '{teacher.first}', '{teacher.last}', '{block}', '{number_to_add}')
+            """
+            self.connection.execute(command)
+            self.connection.commit()
+            return True
+        return False
+    
+    def updateTeacherInClasses(self, teacher: Teacher, block: str, user_id) -> bool:
+        # If teacher does not exist, add them
+        if not self.checkIfInClasses(teacher):
+            self.addTeacherToClasses(teacher, block)
+        command = f"""
+        UPDATE classes SET
+            first='{teacher.first}',
+            last='{teacher.last}',
+            block='{block}',
+            number='{number}'
+        WHERE first='{teacher.first}' AND last='{teacher.last} AND block='{block}'
+        """
+        self.connection.execute(command)
+        self.connection.commit()
+        return True
+
+    # This function should never be used
+    def removeTeacherFromClasses(self, teacher: Teacher, block: str) -> bool:
+        if self.checkIfInClasses(teacher):
+            command = f"""
+            DELETE FROM classes WHERE first='{teacher.first}' AND last='{teacher.last}' AND block='{block}'
+            """
+            self.connection.execute(command)
+            self.connection.commit()
+            return True
+        return False
+    # def addStudentToClasses(self, student: Student) -> bool:
 
     def print_directory(self) -> str:
         string = ""
@@ -134,9 +226,6 @@ class databaseHandler():
         for row in self.cursor.fetchall(): 
             string += str(row) + "\n"
     
-            
-    def __repr__(self) -> str:
-        pass
 
 if __name__ == "__main__":
     NORM = Teacher("RYAN", "NORMANDIN")
@@ -155,7 +244,8 @@ if __name__ == "__main__":
     Kevin.schedule["B"] = CROSBY
     print(Kevin.schedule["B"])
     print(test.updateStudentInDirectory(Kevin))
-    test.getStudentFromDirectory(id=1)
+    print(test.getStudentFromDirectory(Number="6176868207"))
+    print(test.addTeacherToClasses(CROSBY, "B", Kevin_Num))
     # def add_student(self, number, first, last, a, b, c, d, e, f):
     
 # connection = sqlite3.connect("data/absent.db")
